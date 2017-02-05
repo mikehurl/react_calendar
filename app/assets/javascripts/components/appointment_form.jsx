51764ba87d026341757ba128b8319d6e7e7a65ea
Mike Hurl <mikehurl@gmail.com>
2017-02-05 19:27:33 +0000

    adds #handleSubmit method to form component

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -6,11 +6,16 @@ var AppointmentForm = React.createClass( {
         this.props.onUserInput(object);
   },
 
+  handleSubmit: function(event) {
+     event.preventDefault();
+     this.props.onFormSubmit();
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make an appointment</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input name='title' placeholder='Appointment Title' value={this.props.input_title} onChange={this.handleChange} />
           <input name='appointment_time' placeholder='Time and Date' value={this.props.input_appointment_time} onChange={this.handleChange} />
           <input type='submit' value='Make Appointment' />
