8f2054f4b3b179259d4ab2b050149206523946da
Mike Hurl <mikehurl@gmail.com>
2017-02-05 19:10:57 +0000

    changes names of form inputs in initial state so they are picked up properly as form was not displaying in previous code

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,17 +1,17 @@
 var Appointments = React.createClass( {
   getInitialState: function() {
     return {
-      appointments: this.props.appointments.data,
-      input_title: 'Team stand-up',
-      input_appointment_time: 'Tomorrow at 9am'
+      appointments: this.props.appointments,
+      title: 'Team stand-up',
+      appointment_time: 'Tomorrow at 9am'
     }
   },
 
   render: function() {
     return (
       <div>
-        <AppointmentForm input_title={this.state.input_title} input_appointment_time={this.state.input_appointment_time} />
-        <AppointmentsList appointments={this.state .appointments} />
+        <AppointmentForm input_title={this.state.title} input_appointment_time={this.state.appointment_time} />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
