00facad3b3e725950b3999a69aa059dc63090bc9
Mike Hurl <mikehurl@gmail.com>
2017-02-05 19:17:09 +0000

    sets up a #handleChange method to deal with a user inputting data to the form

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,10 +1,17 @@
 var AppointmentForm = React.createClass( {
+  handleChange: function(event) {
+       var name = event.target.name;
+       object = {}
+       object[name] = event.target.value;
+        this.props.onUserInput(object);
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make an appointment</h2>
         <form>
-          <input name='title' placeholder='Appointment Title' value={this.props.input_title} />
+          <input name='title' placeholder='Appointment Title' value={this.props.input_title} onChange={this.handleChange} />
           <input name='appointment_time' placeholder='Time and Date' value={this.props.input_appointment_time} />
           <input type='submit' value='Make Appointment' />
         </form>
